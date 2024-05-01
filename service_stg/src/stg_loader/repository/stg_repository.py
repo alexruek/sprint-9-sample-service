99340519ebb64f4012974a32aa5c080e2eff7f6d
AlexRuEkb <a@pavlyuk.net>
2024-05-01 16:19:09 +0500

    1

---
+++ service_stg/src/stg_loader/repository/stg_repository.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from lib.pg import PgConnect
+
+class StgRepository:
+    def __init__(self, db: PgConnect) -> None:
+        self._db = db
+
+    def order_events_insert(self,
+                            object_id: int,
+                            object_type: str,
+                            sent_dttm: datetime,
+                            payload: str
+                            ) -> None:
+
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                        insert into stg.order_events (object_id, object_type, payload, sent_dttm)
+                        values
+                        on conflict (object_id) do update set id = excluded.object_id;
+                    """,
+                    {
+                        'object_id': object_id,
+                        'object_type': object_type,
+                        'sent_dttm': sent_dttm,
+                        'payload': payload
+                    }
+                )
